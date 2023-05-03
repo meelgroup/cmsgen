bd8710a1b510ece9dc223ea2b7db19288e1a8ea0
Mate Soos <soos.mate@gmail.com>
2023-05-02 20:57:08 -0700

    Better test

--- python/tests/mytest.py
+++ python/tests/mytest.py
@@ -3,14 +3,15 @@ import pycmsgen
 
 a = pycmsgen.Solver()
 a.add_clause([1,2,3,4])
-a.set_var_weight(1, 0.95)
-a.set_var_weight(2, 0.95)
-a.set_var_weight(3, 0.95)
+a.set_var_weight(1, 0.05)
+a.set_var_weight(2, 0.05)
+a.set_var_weight(3, 0.05)
 
 num = [0]*4
-for _ in range(100):
+for at in range(100):
     x = a.solve()
     m = a.get_model()
+    if at==0: print("Example model: ", m)
     for i in range(1, 5) :
         if m[i-1] == i:
             num[i-1]+=1
